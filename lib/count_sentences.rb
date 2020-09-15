4ad745a820af5ef71618fe3a544e3dee03a997e6
Haley Grassi <teacherhaleymarie@gmail.com>
2020-09-15 05:07:43 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,9 @@ class String
     self.end_with?("!")
 
   end
+  
+  #What we'd like to be able to do is call a `count_sentences` method on a string, and get back a, well, count of sentences in that string. This is a string! It has three sentences. Right?".count_sentences. Your `#count_sentences` method should use the `self` keyword to refer to the string on which it is called. 
+
 
   def count_sentences
     sentence_array = self.split(/[.?!]/)
